1d7c17ce21be711653136dd6e07ed6d3c3203cb9
GYANESH ROY <63011408+gnr7@users.noreply.github.com>
2021-04-27 10:15:25 +0530

    Update 002_Add_Two_Numbers.py
    The previous code wasn't giving an optimal solution, moreover it was giving the wrong solution.

--- python/002_Add_Two_Numbers.py
+++ python/002_Add_Two_Numbers.py
@@ -1,4 +1,3 @@
-# Definition for singly-linked list.
 class ListNode(object):
     def __init__(self, x):
         self.val = x
@@ -6,41 +5,11 @@ class ListNode(object):
 
 
 class Solution(object):
-    # def addTwoNumbers(self, l1, l2):
-    #     """
-    #     :type l1: ListNode
-    #     :type l2: ListNode
-    #     :rtype: ListNode
-    #     """
-    #     last = 0
-    #     head = prev = None
-    #     while True:
-    #         if l2 is None and l1 is None and last == 0:
-    #             break
-    #         val = last
-    #         if l2 is not None:
-    #             val += l2.val
-    #             l2 = l2.next
-    #         if l1 is not None:
-    #             val += l1.val
-    #             l1 = l1.next
-    #         if val >= 10:
-    #             val = val % 10
-    #             last = 1
-    #         else:
-    #             last = 0
-    #         current = ListNode(val)
-    #         if prev is None:
-    #             head = current
-    #         else:
-    #             prev.next = current
-    #         prev = current
-    #     return head
-
     def addTwoNumbers(self, l1, l2):
-        carry = 0
-        # dummy head
-        head = curr = ListNode(0)
+
+        dummy = ListNode(0)
+        current, carry = dummy, 0
+
         while l1 or l2:
             val = carry
             if l1:
@@ -49,9 +18,11 @@ class Solution(object):
             if l2:
                 val += l2.val
                 l2 = l2.next
-            curr.next = ListNode(val % 10)
-            curr = curr.next
-            carry = val / 10
-        if carry > 0:
-            curr.next = ListNode(carry)
-        return head.next
+            carry, val = divmod(val, 10)
+            current.next = ListNode(val)
+            current = current.next
+
+        if carry == 1:
+            current.next = ListNode(1)
+
+        return dummy.next
